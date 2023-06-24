56935697e9b594a6650c60b4838cd8148a283718
artem_levin <tema.levin2016@gmail.com>
2023-06-24 17:06:51 +0300

    Add new benchmarks

---
+++ Work№4/H.W#4.py
@@ -0,0 +1,8 @@
+n = 5  # int(input("Количество элементов первого множества: "))
+m = 5  # int(input("Количество элементов второго множества: "))
+list1 = list()
+list2 = list()
+for i in range(n):
+    list1.append(int(input(f"Введите значение {i} множества n: ")))
+for i in range(m):
+    list1.append(int(input(f"Введите значение {i} множества m: ")))
---
+++ Work№6/W№6.py
@@ -0,0 +1,21 @@
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+# for i in range(n):
+#     print(a1 + i * d)
+
+# data = [23, 245, 3465, 457, 45, 65, 341, 24123, 234, 56, 54]
+# print(type(data))
+# min = int(input())
+# max = int(input())
+# i = min
+# for i in range(len(data)):
+#     if min <= data[i] <= max:
+#         print(i)
+
+# list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+# min_number = int(input())
+# max_number = int(input())
+# for i in range(len(list_1)):
+#     if min_number <= list_1[i] <= max_number:
+#         print(i)
